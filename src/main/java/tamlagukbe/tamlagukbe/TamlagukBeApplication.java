ebf9dedb866cad9c134ad3ef6302d8ab45155054
Namgyu11 <103015031+Namgyu11@users.noreply.github.com>
2024-08-06 01:41:33 +0900

    Initial commit

---
+++ src/main/java/tamlagukbe/tamlagukbe/TamlagukBeApplication.java
@@ -0,0 +1,13 @@
+package tamlagukbe.tamlagukbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TamlagukBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TamlagukBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tamlagukbe/tamlagukbe/TamlagukBeApplicationTests.java
@@ -0,0 +1,13 @@
+package tamlagukbe.tamlagukbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TamlagukBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
